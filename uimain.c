cd8dd1b2611768377deb3a0834549fd0c02d82b3
aolivas92 <olivas9215@hotmail.com>
2024-01-31 18:25:45 -0700

    Added the option to print the 5x7 font by choosing 'b'

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, Chars 11x16 = c, Chars 5x7 = b) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -24,14 +24,19 @@ int main()
       puts("You selected square:");
       print_square(5, 5);
       break;
-    case 'c':
-      puts("You selected chars:");
-      for (char c = 'A'; c < 'D'; c++)
-	print_char_11x16(c);
-      break;
     case 'a':
       puts("You selected arrow:");
       print_arrow(5,5);
+      break;
+    case 'c':
+      puts("You selected chars 11x16:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_11x16(c);
+      break;
+    case 'b':
+      puts("You selected chars 5x7:");
+      for (char c = 'A'; c <= 'D'; c++)
+	print_char_5x7(c);
       break;
     case 'q':
       puts("Bye!");
